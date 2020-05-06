9f1cbfd613fed3aa6f96be95115485850bbffa66
Lucas239 <l.almeidasantos239@gmail.com>
2020-05-05 22:25:00 -0300

    entrega final

---
+++ bin/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tempo`.`cidade` (`id`, `nome`, `latitude`, `longitude`) VALUES ('1', 'Sao Paulo',
+'1', '5');
+
+INSERT INTO `tempo`.`cidade` (`id`, `nome`, `latitude`, `longitude`) VALUES ('2', 'Rio de Janeiro',
+'2', '6');
+
+INSERT INTO `tempo`.`cidade` (`id`, `nome`, `latitude`, `longitude`) VALUES ('3', 'Brasilia',
+'3', '7');
